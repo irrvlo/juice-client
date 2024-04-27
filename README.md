218a99466225534b1d39aa6f01951a6f205c7968
irrvlo <51727632+irrvlo@users.noreply.github.com>
2024-04-27 15:55:20 -0500

    1.0.0 yippeee

---
+++ README.md
@@ -0,0 +1,32 @@
+# Juice Client
+
+A light Electron client for Kirka.io. Join our [discord](https://discord.gg/FjzAAdSjng) for support and update notifications.
+
+# Download
+
+- [Website](https://juice.irrvlo.xyz)
+- [Releases](https://github.com/irrvlo/juice-client/releases)
+
+Windows only for now, Linux and MacOS builds will come if its requested enough.
+
+# What features does this have?
+
+- Uncapped FPS
+- Discord Rich Presence
+- more to come :)
+
+## Hotkeys
+| Hotkey | Description |
+| ------ | ----------- |
+| F4 | Return to https://kirka.io |
+| F5 & Ctrl + R| Reload |
+| F11 | Fullscreen |
+| F12 & Ctrl + Shift + I | Open DevTools |
+
+# Is it safe?
+
+Juice Client is 100% completely safe to use. If you face any issues, join our [discord](https://discord.gg/FjzAAdSjng) to report bugs or suggest new features.
+
+## Credits
+
+- me baby WOOOOOO
---
+++ src/main.js
@@ -0,0 +1,119 @@
+require("v8-compile-cache");
+const { app, BrowserWindow, dialog } = require("electron");
+const { autoUpdater } = require("electron-updater");
+const shortcut = require("electron-localshortcut");
+const path = require("path");
+
+let startTimestamp = new Date();
+const rpc = require("discord-rpc");
+const client = new rpc.Client({ transport: "ipc" });
+client.login({ clientId: "1233829658345078846" }).catch(console.error);
+
+app.commandLine.appendSwitch("disable-frame-rate-limit");
+app.commandLine.appendSwitch("disable-gpu-vsync");
+app.allowRendererProcessReuse = true;
+
+autoUpdater.setFeedURL({
+  provider: "github",
+  owner: "irrvlo",
+  repo: "juice-client",
+});
+
+autoUpdater.autoDownload = false;
+
+autoUpdater.on("update-available", () => {
+  const options = {
+    type: "info",
+    buttons: ["Yes", "No"],
+    title: "Update available",
+    message: "An update is available. Would you like to download it?",
+  };
+  dialog.showMessageBox(options).then((response) => {
+    if (response.response === 0) {
+      autoUpdater.downloadUpdate();
+    } else {
+      app.quit();
+    }
+  });
+});
+
+autoUpdater.on("update-downloaded", (info) => {
+  autoUpdater.quitAndInstall();
+});
+
+async function checkForUpdates() {
+  await autoUpdater.checkForUpdates();
+  autoUpdater.checkForUpdatesAndNotify();
+}
+
+function createWindow() {
+  let win = new BrowserWindow({
+    icon: path.join(__dirname, "assets/icon.ico"),
+    title: "Juice Client",
+    width: 1280,
+    height: 720,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  win.removeMenu();
+  win.maximize();
+  win.loadURL("https://kirka.io");
+
+  win.on("page-title-updated", (e) => e.preventDefault());
+
+  function registerShortcut(key, action) {
+    shortcut.register(win, key, action);
+  }
+
+  registerShortcut("Escape", () => {
+    win.webContents.executeJavaScript(`document.exitPointerLock()`);
+  });
+
+  registerShortcut("F4", () => {
+    win.loadURL("https://kirka.io");
+  });
+
+  registerShortcut("F5", () => {
+    win.reload();
+  });
+
+  registerShortcut("Ctrl+R", () => {
+    win.reload();
+  });
+
+  registerShortcut("F11", () => {
+    win.setFullScreen(!win.isFullScreen());
+  });
+
+  registerShortcut("F12", () => {
+    win.webContents.openDevTools();
+  });
+
+  registerShortcut("Ctrl+Shift+I", () => {
+    win.webContents.openDevTools();
+  });
+}
+
+app.on("ready", async () => {
+  checkForUpdates();
+  client.on("ready", () => {
+    client.setActivity({
+      startTimestamp,
+      largeImageKey: "juicer",
+      largeImageText: "Juice Client",
+      buttons: [
+        { label: "Download", url: "https://juice.irrvlo.xyz" },
+        { label: "Discord", url: "https://discord.gg/FjzAAdSjng" },
+      ],
+    });
+  });
+  createWindow();
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
