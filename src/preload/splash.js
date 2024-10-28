dbaddadf557546275402387281549b411f424284
rob <roby.pakalns@gmail.com>
2024-10-28 18:42:04 +0200

    formatted two scripts; replaced require screen to the start

--- src/preload/splash.js
+++ src/preload/splash.js
@@ -1,33 +1,24 @@
-const { ipcRenderer } = require("electron");
-const version = require("../../package.json").version;
+const { ipcRenderer } = require("electron")
+const version = require("../../package.json").version
 
 document.addEventListener("DOMContentLoaded", () => {
-  const versionElement = document.querySelector(".ver");
-  const statusElement = document.querySelector(".status");
+  const versionElement = document.querySelector(".ver")
+  const statusElement = document.querySelector(".status")
 
-  versionElement.textContent = `v${version}`;
+  versionElement.textContent = `v${version}`
 
-  const updateStatus = (status) => {
-    statusElement.textContent = status;
-  };
+  const updateStatus = status => statusElement.textContent = status
 
-  ipcRenderer.send("check-for-updates");
-  updateStatus("Checking for updates...");
+  ipcRenderer.send("check-for-updates")
+  updateStatus("Checking for updates...")
 
-  ipcRenderer.on("update-available", () => {
-    updateStatus("Update available! Downloading...");
-  });
-
-  ipcRenderer.on("update-not-available", () => {
-    updateStatus("No updates available. Launching...");
-  });
+  ipcRenderer.on("update-available", () => updateStatus("Update available! Downloading..."))
+  ipcRenderer.on("update-not-available", () => updateStatus("No updates available. Launching..."))
 
   ipcRenderer.on("update-downloaded", () => {
-    updateStatus("Update downloaded! Installing...");
-    ipcRenderer.send("quit-and-install");
-  });
+    updateStatus("Update downloaded! Installing...")
+    ipcRenderer.send("quit-and-install")
+  })
 
-  ipcRenderer.on("download-progress", (e, progress) => {
-    updateStatus(`Downloading update: ${Math.round(progress.percent)}%`);
-  });
-});
+  ipcRenderer.on("download-progress", (_, progress) => updateStatus(`Downloading update: ${Math.round(progress.percent)}%`))
+})
