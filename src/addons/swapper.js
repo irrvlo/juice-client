718e2ce7d8a459fd882d30c683dd4e03b7bf07a9
SheriffCarry <twitchsheriffcarry+sheriffcarrygithub@gmail.com>
2024-11-29 21:55:06 +0100

    Update swapper.js

--- src/addons/swapper.js
+++ src/addons/swapper.js
@@ -3,9 +3,39 @@ const path = require("path");
 const fs = require("fs");
 const url = require("url");
 
+let common_filenames = [];
+common_filenames.push("success-alert");
+common_filenames.push("bell-store");
+common_filenames.push("destroy");
+common_filenames.push("place");
+common_filenames.push("whoosh");
+common_filenames.push("wound1");
+common_filenames.push("wound2");
+common_filenames.push("open-chest");
+common_filenames.push("reload");
+common_filenames.push("kill1");
+common_filenames.push("kill2");
+common_filenames.push("kill3");
+common_filenames.push("kill4");
+common_filenames.push("kill5");
+common_filenames.push("hit");
+common_filenames.push("gem");
+common_filenames.push("gem2");
+common_filenames.push("coin");
+common_filenames.push("christmas-bell");
+common_filenames.push("button-special");
+common_filenames.push("bullet-dropping2");
+common_filenames.push("bullet-dropping1");
+common_filenames.push("Water");
+common_filenames.push("Stone");
+common_filenames.push("Sand");
+common_filenames.push("Mud");
+common_filenames.push("Grass");
+let regex_filename = /[^\/]+$/gm;
+
 const initResourceSwapper = () => {
   protocol.registerFileProtocol("juiceclient", (request, callback) =>
-    callback({ path: request.url.replace("juiceclient://", "") })
+    callback({ path: request.url.replace("juiceclient://", "") }),
   );
   protocol.registerFileProtocol("file", (request, callback) => {
     callback(decodeURIComponent(request.url.replace("file:///", "")));
@@ -14,7 +44,7 @@ const initResourceSwapper = () => {
   const SWAP_FOLDER = path.join(
     app.getPath("documents"),
     "JuiceClient",
-    "swapper"
+    "swapper",
   );
   const assetsFolder = path.join(SWAP_FOLDER, "assets");
   const folders = ["css", "media", "img", "glb", "js"];
@@ -63,12 +93,38 @@ const initResourceSwapper = () => {
           let filterurl = origfilterurl.replace(/\_/g, "");
           filterurl = filterurl.replace("/", "/*");
           filterurl = filterurl.replace(".", "*.*");
+          let common_name_test = filterurl.match(regex_filename)[0];
+          let common_filepath = common_name_test.split(common_name_test)[0];
+          if (common_name_test) {
+            common_name_test = common_name_test.replace(/\*/gm, "");
+            if (common_filenames.includes(common_name_test.split(".")[0])) {
+              filterurl = `${common_filepath}/*${common_name_test.split(".")[0]}*${common_name_test.split(".")[common_name_test.split(".").length - 1]}`;
+            }
+          }
           swap.filter.urls.push(kirk.replace(origfilterurl, filterurl));
           swap.files[kirk.replace(/\*|_/g, "")] = url.format({
             pathname: filePath,
             protocol: "",
             slashes: false,
           });
+          if (
+            common_filenames.includes(
+              kirk
+                .replace(/\*|_/g, "")
+                .match(/[^\/\\]*?(?=\.[^\/\\]*$)/gm)[0]
+                .replace(/_/gm, ""),
+            )
+          ) {
+            swap.files[
+              kirk
+                .replace(/\*|_/g, "")
+                .replace(/\/([^\/]*?)\.[^.]*\.(\w+)$/, "/$1.$2")
+            ] = url.format({
+              pathname: filePath,
+              protocol: "",
+              slashes: false,
+            });
+          }
         });
       }
     });
@@ -82,10 +138,17 @@ const initResourceSwapper = () => {
       (details, callback) => {
         const redirect =
           "juiceclient://" +
-          (swap.files[details.url.replace(/https|http|(\?.*)|(#.*)|\_/gi, "")] ||
+          (swap.files[
+            details.url.replace(/https|http|(\?.*)|(#.*)|\_/gi, "")
+          ] ||
+            swap.files[
+              details.url
+                .replace(/https|http|(\?.*)|(#.*)|\_/gi, "")
+                .replace(/\/([^\/]*?)\.[^.]*\.(\w+)$/, "/$1.$2")
+            ] ||
             details.url);
         callback({ cancel: false, redirectURL: redirect });
-      }
+      },
     );
   }
 };
